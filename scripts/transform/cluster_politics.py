fb1baee66d2b960e8411021173aa6f755e4c66d1
breval <breval2000@live.fr>
2023-07-06 14:54:24 +0200

    🔧 corriger(data_2017_t1_44_6.py) : déplacer la fonction extract_2017_t1() à l'intérieur du fichier ✨ fonctionnalité(data_2017_t1_44_6.py) : ajouter la fonction extract_2017_t1() pour extraire et filtrer les données de l'élection présidentielle de 2017 pour les départements 44 et 6, puis les insérer dans une base de données SQLite. La fonction extract_2017_t1() a été déplacée à l'intérieur du fichier pour encapsuler le code dans une fonction. Cela permet une meilleure organisation et réutilisabilité du code. La fonction extrait les données du fichier Excel, les filtre pour les départements 44 et 6, puis les insère dans une base de données SQLite. Cela permet de stocker les données de manière structurée
    📦 ajouter(cluster_politics.py) : importer les fonctions d'extraction de données
    Les fonctions d'extraction de données `extract_2022_t1` et `extract_2017_t1` sont importées depuis les fichiers correspondants. Cela permet d'utiliser ces fonctions pour extraire les données nécessaires à la transformation des données politiques.

---
+++ scripts/transform/cluster_politics.py
@@ -0,0 +1,8 @@
+from scripts.Extract.data_2022_t1 import extract_2022_t1
+from scripts.Extract.data_2017_t1_44_6 import extract_2017_t1
+
+df_2022_t1 = extract_2022_t1()
+df_2017_t1 = extract_2017_t1()
+
+
+
